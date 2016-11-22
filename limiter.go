e64fa6702b21a049b0fb1ef7aa313ccb7c84d431
Julian Shen <julianshen@gmail.com>
2016-11-22 22:14:45 +0800

    add error return for invalid requests

--- limiter.go
+++ limiter.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-type RateKeyFunc func(ctx *gin.Context) string
+type RateKeyFunc func(ctx *gin.Context) (string, error)
 
 type RateLimiterMiddleware struct {
 	fillInterval time.Duration
@@ -16,22 +16,30 @@ type RateLimiterMiddleware struct {
 	limiters     map[string]*ratelimit.Bucket
 }
 
-func (r *RateLimiterMiddleware) get(ctx *gin.Context) *ratelimit.Bucket {
-	key := r.ratekeygen(ctx)
+func (r *RateLimiterMiddleware) get(ctx *gin.Context) (*ratelimit.Bucket, error) {
+	key, err := r.ratekeygen(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
 	if limiter, existed := r.limiters[key]; existed {
-		return limiter
+		return limiter, nil
 	}
 
 	limiter := ratelimit.NewBucket(r.fillInterval, r.capacity)
 	r.limiters[key] = limiter
-	return limiter
+	return limiter, nil
 }
 
 func (r *RateLimiterMiddleware) Middleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		limiter := r.get(ctx)
-		if limiter.TakeAvailable(1) == 0 {
-			ctx.AbortWithError(429, errors.New("Too many requests"))
+		limiter, err := r.get(ctx)
+		if err != nil || limiter.TakeAvailable(1) == 0 {
+			if err == nil {
+				err = errors.New("Too many requests")
+			}
+			ctx.AbortWithError(429, err)
 		}
 	}
 }
